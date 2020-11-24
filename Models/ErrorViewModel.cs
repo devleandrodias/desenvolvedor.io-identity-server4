76a1df8a5b1e7b7083b85734777de6cd59a5a284
devleandrodias <leandrodbdias@gmail.com>
2020-11-24 20:43:33 -0300

    adicionando estrutura do identity server 4 template

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace desenvolvedor.io_identity_server4.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
